c7430c481fc18fd93a9ef06e0402789b7b7bd85d
Saad Platini <smplatini@hotmail.com>
2018-10-07 12:16:33 +0200

    Adding text field to UI alert. Updating array

--- TodoeyPart2/TodoListViewController.swift
+++ TodoeyPart2/TodoListViewController.swift
@@ -2,8 +2,10 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Find Mike", "Buy Eggs",
+    var itemArray = ["Find Mike", "Buy Eggs",
                      "Destroy Demogoron"]
+    
+ 
     
  
     override func viewDidLoad() {
@@ -64,6 +66,49 @@ class TodoListViewController: UITableViewController {
   
     
    
+    //MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+      
+        var textField = UITextField()
+        
+        //UI alert window
+        let alert = UIAlertController(title: "Add New Todoey Item ", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            //what will happen once the user clicks the Add Item button on our UIAlert
+            
+           // print(textField.text)
+          
+            self.itemArray.append(textField.text!)
+            
+           //Item array is modified but will not show until we reload data
+            
+            self.tableView.reloadData()
+            
+            
+        }
+        
+        //Adding text field to alert UI
+        alert.addTextField { (alertTextField) in
+            
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+            
+           
+        }
+        
+        
+        //Present alert
+        alert.addAction(action)
+        
+        present(alert, animated : true, completion: nil)
+        
+        
+        
+        
+    }
     
    
     
